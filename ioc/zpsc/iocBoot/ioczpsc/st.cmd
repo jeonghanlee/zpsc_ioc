e576e2052de92bd863b2bfc80fd9012ba99ff40c
Joseph Mead <mead@bnl.gov>
2025-04-22 19:09:42 -0400

    Initial commit

---
+++ ioc/zpsc/iocBoot/ioczpsc/st.cmd
@@ -0,0 +1,80 @@
+#!../../bin/linux-x86_64/zpsc
+
+#- You may have to change zpsc to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+epicsEnvSet("IOCNAME", "lab")
+
+# PSC IP address
+epicsEnvSet("PSC1_IP", "10.0.142.43");  #4009
+epicsEnvSet("BLEN",100000);        # Snapshot DMA Length
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/zpsc.dbd"
+zpsc_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadRecords("db/status10hz.db", "P=$(IOCNAME), NO=1, OFFSET=100")
+
+dbLoadRecords("db/control_glob.db", "P=$(IOCNAME), NO=1")
+dbLoadRecords("db/control_chan.db", "P=$(IOCNAME), NO=1, CHAN=1")
+dbLoadRecords("db/control_chan.db", "P=$(IOCNAME), NO=1, CHAN=2")
+dbLoadRecords("db/control_chan.db", "P=$(IOCNAME), NO=1, CHAN=3")
+dbLoadRecords("db/control_chan.db", "P=$(IOCNAME), NO=1, CHAN=4")
+
+
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=USR, CHAN=1, MSGID=60, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=USR, CHAN=2, MSGID=61, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=USR, CHAN=3, MSGID=62, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=USR, CHAN=4, MSGID=63, BUFLEN=$(BLEN)")
+
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=FLT, CHAN=1, MSGID=70, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=FLT, CHAN=2, MSGID=71, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=FLT, CHAN=3, MSGID=72, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=FLT, CHAN=4, MSGID=73, BUFLEN=$(BLEN)")
+
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=ERR, CHAN=1, MSGID=80, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=ERR, CHAN=2, MSGID=81, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=ERR, CHAN=3, MSGID=82, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=ERR, CHAN=4, MSGID=83, BUFLEN=$(BLEN)")
+
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=INJ, CHAN=1, MSGID=90, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=INJ, CHAN=2, MSGID=91, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=INJ, CHAN=3, MSGID=92, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=INJ, CHAN=4, MSGID=93, BUFLEN=$(BLEN)")
+
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=EVR, CHAN=1, MSGID=100, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=EVR, CHAN=2, MSGID=101, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=EVR, CHAN=3, MSGID=102, BUFLEN=$(BLEN)")
+dbLoadRecords("db/snapshots.db", "P=$(IOCNAME), NO=1, TYPE=EVR, CHAN=4, MSGID=103, BUFLEN=$(BLEN)")
+
+
+#dbLoadRecords("db/fault_ch1.db", "P=$(IOCNAME), NO=1, BUF_LEN=$(BLEN)")
+#dbLoadRecords("db/fault_ch2.db", "P=$(IOCNAME), NO=1, BUF_LEN=$(BLEN)")
+#dbLoadRecords("db/fault_ch3.db", "P=$(IOCNAME), NO=1, BUF_LEN=$(BLEN)")
+#dbLoadRecords("db/fault_ch4.db", "P=$(IOCNAME), NO=1, BUF_LEN=$(BLEN)")
+
+dbLoadRecords("db/wfmstats.db", "P=$(IOCNAME), PSC=1")
+
+
+
+
+var(PSCDebug, 5)	#5 full debug
+
+#psc1 Create the PSC
+createPSC("Tx1", $(PSC1_IP), 7, 0)
+createPSC("Wfm1", $(PSC1_IP), 20, 1)
+createPSC("Rx1", $(PSC1_IP), 600, 1)
+
+
+
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=mead"
